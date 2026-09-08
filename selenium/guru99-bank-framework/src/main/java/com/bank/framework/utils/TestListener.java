7f6ac2c06f84cbc559bee95aa71c4f21ca650a9d
Mandla Qhomane <mandlaqhomane07@gmail.com>
2026-09-08 17:58:59 +0000

    Guru99 bank framework: run in Codespaces, fix TestNG scope and listener

--- selenium/guru99-bank-framework/src/main/java/com/bank/framework/utils/TestListener.java
+++ selenium/guru99-bank-framework/src/main/java/com/bank/framework/utils/TestListener.java
@@ -6,17 +6,17 @@ import org.testng.ITestResult;
 
 public class TestListener implements ITestListener {
 
-    @Override
+    
     public void onTestStart(ITestResult result) {
         ExtentManager.setTest(ExtentManager.getInstance().createTest(result.getMethod().getMethodName()));
     }
 
-    @Override
+    
     public void onTestSuccess(ITestResult result) {
         ExtentManager.getTest().log(Status.PASS, "Passed");
     }
 
-    @Override
+
     public void onTestFailure(ITestResult result) {
         String shot = ScreenshotUtil.capture(result.getMethod().getMethodName());
         ExtentManager.getTest().log(Status.FAIL, result.getThrowable());
@@ -27,7 +27,7 @@ public class TestListener implements ITestListener {
         }
     }
 
-    @Override
+    
     public void onTestSkipped(ITestResult result) {
         ExtentManager.getTest().log(Status.SKIP, "Skipped");
     }
